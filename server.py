51f55398cda1232b8c44540d5e258842c0bd5355
trezes-dev <contatotrezes@gmail.com>
2026-09-16 01:06:01 -0300

    Usa PORT do ambiente

--- server.py
+++ server.py
@@ -949,4 +949,5 @@ if __name__ == "__main__":
         nascer_monstro(c, 5, 5)
     c.commit()
     c.close()
-    app.run(host="0.0.0.0", port=5000)
+import os
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
